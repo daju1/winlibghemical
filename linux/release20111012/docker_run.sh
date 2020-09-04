b84b1c2a18889eec169620fcf2c19fda6b41ed9a
daju1 <daju1@yandex.ru>
2020-09-04 10:04:36 +0300

    work_dir added while docker_run

--- linux/release20111012/docker_run.sh
+++ linux/release20111012/docker_run.sh
@@ -6,10 +6,13 @@ set -x
 DISPLAY=$(env | grep DISPLAY= | sed 's/DISPLAY=//')
 export DISPLAY=$DISPLAY
 PROJECT_ROOT=${PWD}
+work_dir=$(dirname $(dirname "$(pwd)"))/work
+
 DOCKER_IMAGE=ubuntu_ghemical_build:16.04
 
 docker run -it --rm --name ghemical_build_container --cap-add=NET_ADMIN --device /dev/net/tun \
     -v ${PROJECT_ROOT}:${PROJECT_ROOT} \
+    -v ${work_dir}:${work_dir} \
     -v /home/${USER}/.ssh/id_rsa:/home/${USER}/.ssh/id_rsa \
     -v /home/${USER}/.ssh/id_rsa.pub:/home/${USER}/.ssh/id_rsa.pub \
     -v /home/${USER}/.ssh/known_hosts:/home/${USER}/.ssh/known_hosts \
