ce4c337fe44af405cf58983fde73403241007b37
Alexey Drozdov <daju@yandex.ru>
2023-02-20 15:33:21 +0000

    Save frames to the traj from the first one

--- linux/release20111012/examples/example_moldyn_E.03.17.sh
+++ linux/release20111012/examples/example_moldyn_E.03.17.sh
@@ -14,7 +14,7 @@ export LD_LIBRARY_PATH=$lib_dir
 
 infile_gpr="$moldyn_dir/conicnanotube.03/conicnanotube.membrana Ne 14.gpr"
 infile_box="$moldyn_dir/conicnanotube.03/box"
-outfile_traj="$moldyn_dir/conicnanotube.03/first_frame.traj_v17"
+outfile_traj="$moldyn_dir/conicnanotube.03/first_frame_E.traj_v17"
 
 make all
-./example_moldyn -N 2000000000 -f 1000 -v 17 "$infile_gpr" "$infile_box" "$outfile_traj" &
+./example_moldyn -E -N 2000000000 -f 1000 -v 17 "$infile_gpr" "$infile_box" "$outfile_traj" &
