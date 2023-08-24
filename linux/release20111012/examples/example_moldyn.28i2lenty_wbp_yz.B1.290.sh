265c20168bffb5168e7e2e75ede8d076199c1907
Alexey Drozdov <daju@yandex.ru>
2023-08-24 07:22:02 +0000

    28i2lenty_wbp_yz and 28i2lenty_wbp_y example scripts

---
+++ linux/release20111012/examples/example_moldyn.28i2lenty_wbp_yz.B1.290.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+set -x
+
+lib_dir=$(dirname ${PWD})/local/lib/
+moldyn_dir=$(dirname $(dirname $(dirname $(dirname "$(pwd)"))))/moldyn
+
+echo "lib_dir=$lib_dir"
+echo "moldyn_dir=$moldyn_dir"
+
+export PKG_CONFIG_PATH=$(dirname ${PWD})/local/lib/pkgconfig
+export LD_LIBRARY_PATH=$lib_dir
+
+infile_gpr="$moldyn_dir/laboro/new_LC/5CB/28i2lenty_wbp_yz/28i2lenty_wbp_yz.gpr"
+infile_box="$moldyn_dir/laboro/new_LC/5CB/28i2lenty_wbp_yz/28i2lenty_wbp_yz.box"
+outfile_traj="$moldyn_dir/laboro/new_LC/5CB/28i2lenty_wbp_yz/28i2lenty_wbp_yz_B1_290.traj"
+
+./example_moldyn -t 2 -T 290 -B 1 -N 2000000000 -f 5000 -v 19 "$infile_gpr" "$infile_box" "$outfile_traj" &
