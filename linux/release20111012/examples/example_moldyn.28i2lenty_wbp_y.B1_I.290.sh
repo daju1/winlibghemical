2e5be995c01a13137ac5ea06967d85c346874fa5
Alexey Drozdov <daju@yandex.ru>
2023-08-28 04:02:47 +0000

    cumulative_mean_force.patch

--- linux/release20111012/examples/example_moldyn.28i2lenty_wbp_y.B1_I.290.sh
+++ linux/release20111012/examples/example_moldyn.28i2lenty_wbp_y.B1_I.290.sh
@@ -14,6 +14,6 @@ export LD_LIBRARY_PATH=$lib_dir
 
 infile_gpr="$moldyn_dir/laboro/new_LC/5CB/28i2lenty_wbp_y/28i2lenty_wbp_y.gpr"
 infile_box="$moldyn_dir/laboro/new_LC/5CB/28i2lenty_wbp_y/28i2lenty_wbp_y.box"
-outfile_traj="$moldyn_dir/laboro/new_LC/5CB/28i2lenty_wbp_y/28i2lenty_wbp_y B1 I 290.traj"
+outfile_traj="$moldyn_dir/laboro/new_LC/5CB/28i2lenty_wbp_y/28i2lenty_wbp_y_B1_I_290.traj"
 
 ./example_moldyn -t 2 -T 290 -I -B 1 -N 2000000000 -f 5000 -v 19 "$infile_gpr" "$infile_box" "$outfile_traj" &
