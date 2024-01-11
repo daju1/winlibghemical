f9c495d4b5aefaa5df603d41062303b9256d47c9
Alexey Drozdov <daju@yandex.ru>
2024-01-11 23:29:23 +0000

    moldyn_nanotube12_cyanoform4 fix

--- linux/release20111012/examples/moldyn_nanotube12_cyanoform4_B0_E.sh
+++ linux/release20111012/examples/moldyn_nanotube12_cyanoform4_B0_E.sh
@@ -12,8 +12,8 @@ echo "moldyn_dir=$moldyn_dir"
 export PKG_CONFIG_PATH=$(dirname ${PWD})/local/lib/pkgconfig
 export LD_LIBRARY_PATH=$lib_dir
 
-infile_gpr="$moldyn_dir/nanotube_12_membrane_6_tricyanomethyl/12.0_4cyanoforms_O2.gpr"
-infile_box="$moldyn_dir/nanotube_12_membrane_6_tricyanomethyl/box4"
-outfile_traj="$moldyn_dir/nanotube_12_membrane_6_tricyanomethyl/12.0_4cyanoforms_O2.B0_E.traj"
+infile_gpr="$moldyn_dir/nanotube_12_membrane_4_tricyanomethyl/12.0_4cyanoforms_O2.gpr"
+infile_box="$moldyn_dir/nanotube_12_membrane_4_tricyanomethyl/box4"
+outfile_traj="$moldyn_dir/nanotube_12_membrane_4_tricyanomethyl/12.0_4cyanoforms_O2.B0_E.traj"
 
 ./example_moldyn -E -B 0 -N 2000000000 -f 1000 -v 17 -t 2 "$infile_gpr" "$infile_box" "$outfile_traj" &
