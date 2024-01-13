1e34c66d6e86aaa2cabc58728e52568a3154dc07
Alexey Drozdov <daju@yandex.ru>
2024-01-13 04:00:22 +0000

    moldyn_nanotube12_cyanoform4.sh edited with P=const

--- linux/release20111012/examples/moldyn_nanotube12_cyanoform4.sh
+++ linux/release20111012/examples/moldyn_nanotube12_cyanoform4.sh
@@ -3,11 +3,11 @@
 set -e
 set -x
 
-# ./moldyn_nanotube12_cyanoform4_B0_E.sh &
-# ./moldyn_nanotube12_cyanoform4_B0_E_I.sh &
-./moldyn_nanotube12_cyanoform4_B0_T.sh &
-./moldyn_nanotube12_cyanoform4_B0_T_I.sh &
-# ./moldyn_nanotube12_cyanoform4_B2_E.sh &
-# ./moldyn_nanotube12_cyanoform4_B2_E_I.sh &
-./moldyn_nanotube12_cyanoform4_B2_T.sh &
-./moldyn_nanotube12_cyanoform4_B2_T_I.sh &
+./moldyn_nanotube12_cyanoform4_B0_E_P.sh &
+./moldyn_nanotube12_cyanoform4_B0_E_P_I.sh &
+./moldyn_nanotube12_cyanoform4_B0_T_P.sh &
+./moldyn_nanotube12_cyanoform4_B0_T_P_I.sh &
+./moldyn_nanotube12_cyanoform4_B2_E_P.sh &
+./moldyn_nanotube12_cyanoform4_B2_E_P_I.sh &
+./moldyn_nanotube12_cyanoform4_B2_T_P.sh &
+./moldyn_nanotube12_cyanoform4_B2_T_P_I.sh &
