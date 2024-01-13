144f634b0cfd24f2a356345aac0a9f7b3b51b421
Alexey Drozdov <daju@yandex.ru>
2024-01-13 03:44:00 +0000

    moldyn_nanotube12_cyanoform4 with P=const scripts added

---
+++ linux/release20111012/examples/moldyn_nanotube12_cyanoform4_B2_T_P_I.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+set -x
+
+lib_dir=$(dirname ${PWD})/local/lib/
+moldyn_dir=$(dirname $(dirname $(dirname $(dirname "$(pwd)"))))/moldyn
+
+echo "lib_dir=$lib_dir"
+echo "moldyn_dir=$moldyn_dir"
+
+export PKG_CONFIG_PATH=$(dirname ${PWD})/local/lib/pkgconfig
+export LD_LIBRARY_PATH=$lib_dir
+
+infile_gpr="$moldyn_dir/nanotube_12_membrane_4_tricyanomethyl/12.0_4cyanoforms_O2_.gpr"
+infile_box="$moldyn_dir/nanotube_12_membrane_4_tricyanomethyl/box4"
+outfile_traj="$moldyn_dir/nanotube_12_membrane_4_tricyanomethyl/12.0_4cyanoforms_O2_.B2_T_P_I.traj"
+
+./example_moldyn -P -I -B 2 -N 2000000000 -f 1000 -v 17 -t 2 "$infile_gpr" "$infile_box" "$outfile_traj" &
