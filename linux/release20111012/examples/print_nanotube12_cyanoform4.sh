e3e1542d62325c4cf75f9f85845de100b9c6e213
Alexey Drozdov <daju@yandex.ru>
2024-01-14 22:34:30 +0000

    print_nanotube12_cyanoform4.sh script updated with cycle

--- linux/release20111012/examples/print_nanotube12_cyanoform4.sh
+++ linux/release20111012/examples/print_nanotube12_cyanoform4.sh
@@ -3,11 +3,28 @@
 set -e
 set -x
 
-./print_nanotube12_cyanoform4_B0_E.sh
-./print_nanotube12_cyanoform4_B0_E_I.sh
-./print_nanotube12_cyanoform4_B0_T.sh
-./print_nanotube12_cyanoform4_B0_T_I.sh
-./print_nanotube12_cyanoform4_B2_E.sh
-./print_nanotube12_cyanoform4_B2_E_I.sh
-./print_nanotube12_cyanoform4_B2_T.sh
-./print_nanotube12_cyanoform4_B2_T_I.sh
+lib_dir=$(dirname ${PWD})/local/lib/
+moldyn_dir=$(dirname $(dirname $(dirname $(dirname "$(pwd)"))))/moldyn.remote
+
+echo "lib_dir=$lib_dir"
+echo "moldyn_dir=$moldyn_dir"
+
+export PKG_CONFIG_PATH=$(dirname ${PWD})/local/lib/pkgconfig
+export LD_LIBRARY_PATH=$lib_dir
+
+folder="$moldyn_dir/nanotube_12_membrane_4_tricyanomethyl"
+
+infile_gpr="$folder/12.0_4cyanoforms_O2_.gpr"
+infile_box="$folder/box4"
+
+dim=2
+
+for infile_traj in $(ls $folder/*.traj); do
+
+    rm -f $infile_traj.plots.dim$dim.gas.txt
+    rm -f $infile_traj.plots.dim$dim.membrane.txt
+
+    ./example_make_mol_plots -d $dim -t 0 "$infile_gpr" "$infile_box" "$infile_traj" >> $infile_traj.plots.dim$dim.gas.txt
+    ./example_make_mol_plots -d $dim -t 1 "$infile_gpr" "$infile_box" "$infile_traj" >> $infile_traj.plots.dim$dim.membrane.txt
+
+done
