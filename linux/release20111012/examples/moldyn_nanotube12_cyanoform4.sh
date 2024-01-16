357278d121b1ff3a171b1996aa836ab589a2dfb1
Alexey Drozdov <daju@yandex.ru>
2024-01-16 15:27:13 +0000

    moldyn_nanotube12_cyanoform4.sh edited

--- linux/release20111012/examples/moldyn_nanotube12_cyanoform4.sh
+++ linux/release20111012/examples/moldyn_nanotube12_cyanoform4.sh
@@ -11,3 +11,13 @@ set -x
 ./moldyn_nanotube12_cyanoform4_B2_T_I.sh &
 ./moldyn_nanotube12_cyanoform4_B2_T_P.sh &
 ./moldyn_nanotube12_cyanoform4_B2_T_P_I.sh &
+
+./moldyn_nanotube12_cyanoform4_B0_H.sh &
+./moldyn_nanotube12_cyanoform4_B0_H_I.sh &
+./moldyn_nanotube12_cyanoform4_B0_H_P.sh &
+./moldyn_nanotube12_cyanoform4_B0_H_P_I.sh &
+./moldyn_nanotube12_cyanoform4_B2_H.sh &
+./moldyn_nanotube12_cyanoform4_B2_H_I.sh &
+./moldyn_nanotube12_cyanoform4_B2_H_P.sh &
+./moldyn_nanotube12_cyanoform4_B2_H_P_I.sh &
+
