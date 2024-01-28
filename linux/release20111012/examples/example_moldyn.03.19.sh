2de33cbf81aff98eb6a4a8f225cec0af4940c15c
Alexey Drozdov <daju@yandex.ru>
2024-01-28 14:02:42 +0300

    example_moldyn.03.19.sh edited with Nose Hoover

--- linux/release20111012/examples/example_moldyn.03.19.sh
+++ linux/release20111012/examples/example_moldyn.03.19.sh
@@ -14,7 +14,7 @@ export LD_LIBRARY_PATH=$lib_dir
 
 infile_gpr="$moldyn_dir/conicnanotube.03/conicnanotube.membrana Ne 14.gpr"
 infile_box="$moldyn_dir/conicnanotube.03/box"
-outfile_traj="$moldyn_dir/conicnanotube.03/v19/first_frame.traj_v19"
+outfile_traj="$moldyn_dir/conicnanotube.03/H.traj"
 
 make all
-./example_moldyn -N 2000000000 -f 1000 -v 17 "$infile_gpr" "$infile_box" "$outfile_traj" &
+./example_moldyn -H -N 2000000000 -f 1000 -v 17 "$infile_gpr" "$infile_box" "$outfile_traj" &
