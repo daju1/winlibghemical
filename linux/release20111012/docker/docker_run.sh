3ed351650dc7c32936549aed9727acacc7352a46
Alexey Drozdov <daju@yandex.ru>
2024-03-11 11:05:23 +0200

    articles folder added in docker_run.sh

--- linux/release20111012/docker/docker_run.sh
+++ linux/release20111012/docker/docker_run.sh
@@ -6,7 +6,7 @@ set -x
 DISPLAY=$(env | grep DISPLAY= | sed 's/DISPLAY=//')
 export DISPLAY=$DISPLAY
 PROJECT_ROOT=$(dirname $(dirname $(dirname ${PWD})))
-workspace_dir=$(dirname $(dirname $(dirname "$(pwd)")))/workspace
+workspace_dir=$(dirname $(dirname $(dirname "$(pwd)")))/articles
 workspace_dir2=$(dirname $(dirname $(dirname $(dirname "$(pwd)"))))/moldyn
 
 DOCKER_IMAGE=ubuntu_ghemical_build:16.04
@@ -19,7 +19,7 @@ docker run -it --rm --name ghemical_build_container --cap-add=NET_ADMIN --device
     -v /home/${USER}/.ssh/id_rsa:/home/${USER}/.ssh/id_rsa \
     -v /home/${USER}/.ssh/id_rsa.pub:/home/${USER}/.ssh/id_rsa.pub \
     -v /home/${USER}/.ssh/known_hosts:/home/${USER}/.ssh/known_hosts \
-    -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -e DISPLAY=:0 --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --user ${USER}:${USER} ${DOCKER_IMAGE} /bin/bash
 
 
